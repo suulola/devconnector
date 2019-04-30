34b59682f5d3959cd39ddf9e7b425bb810197590
mySuulola <oluwaseyisuulola@gmail.com>
2019-04-30 18:08:32 +0100

    basic setup of react-router-dom and the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Landing from './components/Landing';
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Landing} />
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <footer className='mt-5 text-center bg-dark text-white p-4'>
+        Copyright &copy; {new Date().getFullYear()} DevConnector
+      </footer>
+  )
+}
+
+export default Footer
